fbc0e7ed9faea5623acfe166c3a778cbef04d981
노수인 <bigpie1367@gmail.com>
2021-07-11 13:35:16 +0000

    Solved 0x02 at 2021.07.11

---
+++ src/problems/boj10869.java
@@ -0,0 +1,29 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a, b, result;
+        
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        result = a+b;
+        System.out.println(result);
+        
+        result = a-b;
+        System.out.println(result);
+        
+        result = a*b;
+        System.out.println(result);
+        
+        result = a/b;
+        System.out.println(result);
+        
+        result = a%b;
+        System.out.println(result);
+    }
+}
---
+++ src/problems/boj10871.java
@@ -0,0 +1,24 @@
+package problems;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        ArrayList<Integer> listA = new ArrayList<>();
+        int N, X;
+        
+        N = sc.nextInt();
+        X = sc.nextInt();
+        
+        for (int i = 0; i < N; i++) {
+            listA.add(sc.nextInt());
+        }
+        
+        for (int num : listA) {
+            if (num < X) System.out.printf("%d ", num);
+        }
+    }
+}
