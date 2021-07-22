2fd824a5a9353a632a73c62a2c0e3b1cc79afaad
노수인 <bigpie1367@gmail.com>
2021-07-22 13:54:37 +0000

    solved 0x02 at 2021.07.22.

---
+++ src/problems/boj2440.java
@@ -0,0 +1,28 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = N - i; j > 0; j--) {
+                bw.write("*");
+            }
+            
+            bw.write("\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/problems/boj2444.java
@@ -0,0 +1,35 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - i - 1; j++) bw.write(" ");
+            for (int j = 0; j < i * 2 + 1; j++) bw.write("*");
+            bw.write("\n");
+        }
+        
+        for (int i = 0; i < N * 2 - 1; i++) bw.write("*");
+        bw.write("\n");
+        
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < i + 1; j++) bw.write(" ");
+            for (int j = 0; j < (N - i - 1) * 2 - 1; j++) bw.write("*");
+            bw.write("\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
